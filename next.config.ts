85aae63aed8cfe9af5ebcf60ddca0c49f12dc817
Gabrielle <piresgaby0@gmail.com>
2026-05-16 15:28:36 -0300

    corrigindo next.config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactStrictMode: true,
   devIndicators: false,
+  /* config options here */
+  webpack: (config, { isServer }) => {
+    // Se estiver compilando para o navegador (client-side), ignora o módulo 'fs'
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
